f14c0f72218236b42efb8d736bf5ffb864a528d6
Savannah Mercedes <savannahmunn1996@gmail.com>
2023-05-15 15:32:02 -0700

    Set up and Testing tailwind

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+ 
+  return (
+   
+     <div className="w-screen h-screen bg-blue-800">
+      <h1 className="font-robo text-pink-700" >hello I am happy</h1>
+     </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{html,js,jsx}"
+  ],
+  theme: {
+    extend: {
+
+      fontFamily: {
+      "robo" : ["Roboto", "sans-serif"]
+    
+    },
+    },
+  },
+  plugins: [],
+
+}
