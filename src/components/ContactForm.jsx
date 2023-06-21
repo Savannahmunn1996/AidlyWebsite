51e096a44a414bc88faa0944d6ab5c241a1d36ba
Savannah Mercedes <savannahmunn1996@gmail.com>
2023-06-21 14:45:23 -0700

    thankyou render

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import axios from 'axios';
-import Modal from "./Modal"
+
+import Thankyou from './Thankyou';
 
 
 const ContactForm = () => {
@@ -8,17 +9,17 @@ const ContactForm = () => {
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
   // const [heyModal, setModal] = useState(false);
-
+  const [thanks, setThanks] = useState("");
 
   function sendMail() {
-   
+
     if (email && message) {
       // This is a request to the server
       axios.post("http://localhost:3000/send-email", {
         message,
         email
       })
-        .then(() => setModal(true))
+        .then(() => setThanks(true))
         .catch(() => alert("nope, try again..."))
       return;
     }
@@ -27,22 +28,81 @@ const ContactForm = () => {
     }
   }
 
-  return (
-    <>
-      <form >
+ 
+   
+     
+        {/* <form>
+          <div className="items-center grid grid-cols-1">
+            <label htmlFor="message" className="text-4xl text-gray-700 font-semibold ml-8 mb-10">
+              Dear Aidly team,
+            </label>
+            <input
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              type="text"
+              className="focus:outline-gray-300 h-40 bg-gray-200"
+              placeholder="Enter Message"
+            ></input>
+            <input
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="mb-10 h-28 focus:outline-gray-300 bg-gray-200"
+              placeholder="Enter Email"
+            ></input>
+            <button
+              onClick={() => sendMail()}
+              className="text-Dpurp rounded-[18px] outline-2 md:outline-4 outline outline-Dpurp w-40 h-10 ml-8 mb-14 hover:bg-gray-200"
+            >
+              Send Message
+            </button>
+          </div>
+        </form>
+      
+       <Thankyou close={() => setThanks(false)} />
+     */}
+
+
+let formOrThankyou;
+  if (!thanks) {
+    formOrThankyou = (
+      <form>
         <div className="items-center grid grid-cols-1">
-          <label htmlFor="message" className="text-4xl text-gray-700 font-semibold ml-8 mb-10">
+          <label
+            htmlFor="message"
+            className="text-4xl text-gray-700 font-semibold ml-8 mb-10"
+          >
             Dear Aidly team,
-          </label >
-          <input value={message} onChange={(e) => setMessage(e.target.value)} type="text" className=" focus:outline-gray-300 h-40 bg-gray-200" placeholder='Enter Message'></input>
-          <input value={email} onChange={(e) => setEmail(e.target.value)} className='mb-10 h-28 focus:outline-gray-300 bg-gray-200' placeholder='Enter Email'></input>
-          <button onClick={() => sendMail()} className='text-Dpurp rounded-[18px] outline-2 md:outline-4 outline outline-Dpurp w-40 h-10 ml-8 mb-14 hover:bg-gray-200'>Send Message</button>
+          </label>
+          <input
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            type="text"
+            className="focus:outline-gray-300 h-40 bg-gray-200"
+            placeholder="Enter Message"
+          ></input>
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="mb-10 h-28 focus:outline-gray-300 bg-gray-200"
+            placeholder="Enter Email"
+          ></input>
+          <button
+            onClick={() => sendMail()}
+            className="text-Dpurp rounded-[18px] outline-2 md:outline-4 outline outline-Dpurp w-40 h-10 ml-8 mb-14 hover:bg-gray-200"
+          >
+            Send Message
+          </button>
         </div>
       </form>
-      {/* <Modal isVisible={heyModal} close={() => setModal(false)} /> */}
+    );
+  } else {
+    formOrThankyou = <Thankyou close={() => setThanks(false)} />;
+  }
 
-    </>
-  )
-}
+  return <>{formOrThankyou}</>;
+};
+   
+ 
+
 
 export default ContactForm
---
+++ src/components/Thankyou.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Thankyou = ({  close }) => {
+
+ 
+
+
+
+
+
+    return (
+        <div className=' flex justify-center items-center  '>
+        <div className=" w-[200px] md:w-[400px] flex flex-col justify-center items-center ">
+
+            <button className="text-white text-2xl place-self-end" onClick={() => close()}>X</button>
+            <div className=' p-2  h-60 w-60 md:w-96  '>
+                <img className='mt-2 ml-4 h-20 w-20' src="/bubbleIcon.png" alt="aidly icon" />
+                <h1 className='text-2xl mt-3 ml-4 font-medium text-gray-800'>Thanks for reaching out!</h1>
+                <p className='text-gray-800 mt-2 ml-4'>We got your message. We'll be in touch soon.</p></div>
+
+
+
+        </div>
+        </div>
+    )
+}
+
+export default Thankyou
