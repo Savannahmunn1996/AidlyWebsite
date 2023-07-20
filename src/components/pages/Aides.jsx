46e78ee63d7441be44964f830ea3a918f46c1f1d
Savannah Mercedes <savannahmunn1996@gmail.com>
2023-07-19 18:55:23 -0700

    footer for Aides

--- src/components/pages/Aides.jsx
+++ src/components/pages/Aides.jsx
@@ -1,17 +1,18 @@
 
-import React from 'react'
+import React, { useEffect } from 'react'
 import InfoCards from '../InfoCards'
 import { useLocation } from 'react-router-dom';
 import Foot from "../Foot"
 
-const Aides = ({theme, darkMode, setTheme}) => {
+
+const Aides = ({ theme, darkMode, setTheme }) => {
 
   // const [currentIt, setIt] = useState();
 
 
   // const handleChange = (aide) => {
   //   setIt(aide);
-    
+
   // };
 
   //I am thinking about using this if statement to render each aides component and I
@@ -19,43 +20,50 @@ const Aides = ({theme, darkMode, setTheme}) => {
 
   // const Render = () => {
   //   if (currentIt === 'Home') {
-  
+
   //   }
   //   if (currentIt === 'Aides') {
-     
+
   //   }
   //   if (currentIt === 'Contact') {
-    
+
   //   }
   //   if (currentIt === 'Contact') {
-    
+
   //   }
   //   if (currentIt === 'Contact') {
-    
+
   //   }
   //   if (currentIt === 'Contact') {
-    
+
   //   }
   //   if (currentIt === 'Contact') {
-    
+
   //   }
 
   //   return <Atlan/>;
   // };
+  const location = useLocation();
 
 
-  
+  useEffect(() => {
+    // Set body class based on current route
+    document.body.classList.add('aides-page');
+    return () => {
+      document.body.classList.remove('aides-page');
+    };
+  }, []);
 
 
 
-// This was originally where
-//text-${darkMode ? 'white' : 'white'} this was in there before
+  // This was originally where
+  //text-${darkMode ? 'white' : 'white'} this was in there before
 
   return (
     <div className={`bg-${darkMode ? 'black' : 'black'}  min-h-screen  `} >
-      <InfoCards theme={theme} setTheme={setTheme}/>
+      <InfoCards theme={theme} setTheme={setTheme} />
       {/* <Foot isTheAides={isTheAides}></Foot> */}
-      </div>
+    </div>
   )
 }
 
